54c2ed418dc7a7b6aea138c82176093567a84861
Tae Kim <taekim.dev@gmail.com>
2025-05-30 22:57:24 +0800

    add default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,1 +1,28 @@
- 
+<template>
+  <div>
+    <nav>
+      <NuxtLink v-for="locale in config.public.locales" 
+                :key="locale" 
+                :to="`/${locale}/international`"
+                class="locale-link">
+        {{ locale }}
+      </NuxtLink>
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+</script>
+
+<style scoped>
+.locale-link {
+  margin-right: 10px;
+  text-decoration: none;
+  color: #333;
+}
+.locale-link:hover {
+  text-decoration: underline;
+}
+</style> 
